b2de4de823149604c11ec9357c7d8814dde3043b
Micaela <eladelmonte@gmail.com>
2022-07-28 22:52:17 +0900

    Deploy testing for API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,7 +7,7 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json(req)
+  res.status(200).json({ name: 'John Doe' })
 }
